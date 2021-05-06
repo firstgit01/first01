7329ae1d2c731c16e9190f2248e1de309e4795f7
firstgit01 <81025903+firstgit01@users.noreply.github.com>
2021-05-06 09:58:14 +0530

    edited the file for pull request
    added variable and to take input and display it.

--- first-c++.cpp
+++ first-c++.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main()
 {
+    double b;
     cout<<"HELLO GITHUB\nThis is my first program to be uploded on github\n";
     cout<<"Edited this file in firstgit acount from github\n";
+    cin>>b;
+    cout<<b;
     return 0;
 }
